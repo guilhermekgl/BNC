535655dde36b4430b73a6cc24578aa0444e110e3
Guilherme Reis <72157788+guilhermekgl@users.noreply.github.com>
2022-07-13 15:41:11 -0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,11 +1,17 @@
 from datetime import date
 from datetime import datetime
 
+# Especifição da data para a formatação PT-BR e de date para string
+
 def date_para_str(data: date) -> str:
     return data.strtime('%D/%M/Y')
 
+# Formatação de string para Date
+
 def str_para_date(data: str) -> date:
     return datetime.strptime(data, '%D/%M/%y')
+
+# De float para string
 
 def formata_float_str(valor: float) -> str:
     return f'R$ {valor:, .2f}'
